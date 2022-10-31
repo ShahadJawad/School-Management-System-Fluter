9658d057252a8d0663664441b5523d010adde5a4
AliBahaa <alibahaa963@gmail.com>
2022-10-31 22:32:51 +0300

    splash_screen2

--- lib/pagess/splash_screen.dart
+++ lib/pagess/splash_screen.dart
@@ -4,35 +4,55 @@ import 'package:flutter/material.dart';
 import 'package:grad_projct/pagess/login_page.dart';
 
 class Splash_Screen extends StatefulWidget {
-  const Splash_Screen({super.key});
+  const Splash_Screen({super.key,});
+ 
 
   @override
   State<Splash_Screen> createState() => _Splash_ScreenState();
 }
 
 class _Splash_ScreenState extends State<Splash_Screen> {
-  @override
-  void inistate() {
+  void initState() {
     super.initState();
-    Timer(const Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (_) => const login(),
-        ),
-      );
-    });
+    startSplashScreenTimer();
   }
 
+  startSplashScreenTimer() async {
+    var _duration = new Duration(seconds: 5);
+    return new Timer(_duration, navigationToNextPage);
+  }
+
+  void navigationToNextPage() async {
+    Navigator.pushAndRemoveUntil(
+      context, new MaterialPageRoute(builder: (context) => new login()),
+      (Route<dynamic> route) => false, //
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         backgroundColor: Color.fromARGB(255, 179, 163, 238),
         body: Center(
-          child: Text(
-            'مدرستي',
-            style: TextStyle(
-              fontSize: 40,
-              color: Colors.white,
-            ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.width / 1,
+              ),
+              const Text(
+                'مدرستي',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width / 2,
+              ),
+              const CircularProgressIndicator(
+                backgroundColor: Colors.white,
+              )
+            ],
           ),
         ));
   }
