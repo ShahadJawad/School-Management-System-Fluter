109acd7b51dea1641d6673f91409f0bbf068b1ca
LIINWARE <97309058+ShahadJawad@users.noreply.github.com>
2022-11-05 20:29:27 +0300

    home1

---
+++ lib/lists/colorList.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:grad_projct/constant.dart';
+
+class colors {
+  colors({required this.myColors});
+  Color myColors;
+}
+
+List<colors> colorList =[
+ colors(myColors: KColor),
+  colors(myColors: Colors.pinkAccent),
+  colors(myColors: Colors.teal),
+  colors(myColors: Colors.deepPurple),
+  colors(myColors: KColor),
+  colors(myColors: Colors.pinkAccent),
+  colors(myColors: Colors.teal),
+  colors(myColors: Colors.deepPurple)
+];
